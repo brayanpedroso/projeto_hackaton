f5285c36820e6cbab7d5cb50f56b711a34890b4c
= <=>
2023-10-19 18:33:13 -0300

    att_02

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,71 @@
 
 <template>
   <div>
-  <img id="logo" src="@/assets/img/m1.png" :alt="as">
+    <div id="logo-url">
+      <img id="logo" alt="Vue logo" class="logo" src="src\assets\img\m1.png" width="149" height="110" />
+    </div>
     <nav class="nav">
+      <div><router-link to="/">Home</router-link></div>
+      <div><router-link to="/pisos-item">pisos</router-link></div>
+      <div><router-link to="/contratar-item">contratar</router-link></div>
       
-      <router-link to="/">Home</router-link>
-      <router-link to="/pisos-item">pisos</router-link>
-      <router-link to="/contratar-item">contratar</router-link>
-     
     </nav>
     <router-view />
   </div>
 </template>
 
 <style scoped>
- .nav{
-  background: linear-gradient(183deg, rgba(155, 155, 155, 0.54) 0%, rgba(217, 217, 217, 0.00) 100%);
+ #logo-url {
+    margin: auto;
+    margin-left: 0;
+    margin-top: -105%;
+    margin-left: -125%;
+  }
+  .nav {
+    background: linear-gradient(183deg, rgba(155, 155, 155, 0.54) 0%, rgba(217, 217, 217, 0.00) 100%);
+    padding: 15px 50px;
     display: flex;
     justify-content: flex-end;
     align-items: center;
-    width: 445%;
+    width: 424%;
     height: 100px;
     flex-shrink: 0;
-    margin-top: -129%;
-    margin-left: -114%;
-    
-}
-
-a { 
-  color: inherit; 
-  text-decoration: none;
-  width: 445%;
-  line-height: normal;
-  font-family: Inter;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 400;
-  width: 20%;
-  height: 71px;
-  margin-left: 10%;
-  color: #000000;
-
-} 
-#logo {
-    width: 70px;
-    height: 70px;
-   
+    margin-left: -95%;
+    margin-top:-30%; 
   }
-
+  a {
+  text-decoration: none;
+  color: #fff;
+  width: 75px;
+  font-size: 32px;
+  font-weight: 400;
+  font-family: Inter;
+  line-height: normal;
+  font-style: normal;
+ margin-left: -900%;
+}
+  * {
+      font-family: Helvetica;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+      
+        
+    }
+    .main-container {
+      margin: 50px;
+      min-height: 250px;
+    }
+  
+ 
+  
+    h1 {
+      text-align: center;
+      font-size: 42px;
+      margin-bottom: 30px;
+      color: #222;
+    }
+  
 
 
 
--- src/views/home-item.vue
+++ src/views/home-item.vue
@@ -5,12 +5,58 @@
        res
       pisos
     </p>
-  
+  <button class="button"><a href="/">Confira</a></button>
    
   
   
-     <button class="button"><a href="/">Confira</a></button>
+     
     
    
   </div>
   </template>
+
+  <style scoped>
+.h1{
+    width: 100px;
+  height: 80px;
+  flex-shrink: 0;
+  color: #ffffff;
+  font-family: Inter;
+  font-size: 128px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-left: -250px;
+  margin-top: 10%;
+  }
+  .button{
+   margin-top: 30%;
+   width: 158px;
+  height: 49px;
+  flex-shrink: 0;
+  background: #DF2A2A;
+  margin-left: 35%;
+  color: #FFF;
+  font-family: Inter;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  }
+  
+
+  a { color: white;
+   text-decoration: none } 
+  a:link {
+  text-decoration:none;
+  }
+  a:visited {
+  text-decoration:none;
+  }
+  a:hover {
+  text-decoration:underline;
+  text-decoration: none
+  }
+  
+
+</style>
